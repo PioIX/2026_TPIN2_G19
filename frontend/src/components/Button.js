c925dfff3e458e0621d8a37f1ccf5fa7ee7fe2c9
fngarcia <fngarcia@pioix.edu.ar>
2026-09-21 13:29:59 -0300

    Cambios

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Button({
+  children,
+  onClick,
+  type = "button",
+  disabled = false,
+}) {
+  return (
+    <button type={type} onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+}
